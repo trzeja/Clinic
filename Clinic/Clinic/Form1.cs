d4586fa6c83fbc03b6c99eec48daa90bf668dc84
michal <mysz102@hotmail.com>
2017-07-12 12:39:46 +0200

    md5

--- Clinic/Clinic/Form1.cs
+++ Clinic/Clinic/Form1.cs
@@ -4,6 +4,7 @@ using System.ComponentModel;
 using System.Data;
 using System.Drawing;
 using System.Linq;
+using System.Security.Cryptography;//do md5
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -33,7 +34,21 @@ namespace Clinic
         private void button1_Click(object sender, EventArgs e)
         {
             // skrót hasła i pozostała magia
-            loginUsernameTextBox.Text = loginPasswordTextBox.Text;
+            loginUsernameTextBox.Text = MD5Hash(loginPasswordTextBox.Text);// to tylko przykład/do testowania
+        }
+        public string MD5Hash(string input)
+        {
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.ASCII.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+            StringBuilder sb = new StringBuilder();
+
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));//X2 = upper-case, x2 = lower-case
+            }
+
+            return sb.ToString();
         }
     }
 }
