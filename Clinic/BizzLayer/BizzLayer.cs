bc01342dc374477d7a28d8a01a84d8c2a932adee
trzej_000 <trzeja.mikolaj@gmail.com>
2017-07-29 18:57:13 +0200

    Wywalenie smieci

--- Clinic/BizzLayer/BizzLayer.cs
+++ Clinic/BizzLayer/BizzLayer.cs
@@ -9,5 +9,6 @@ using System.Security.Cryptography;
 namespace BizzLayer
 {       
     
-    
+    //klasa zbyt ogolna, raczej nie traktujcie jej jako worka na logike tylko starajcie sie twoarzyc klasy i foldery.
+    //jesli nie macie pomyslu to wrzucajcie tu w ostatecznosci
 }
