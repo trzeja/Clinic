5b923e653359cc51d0c9d70cb62bd6d76a012fd3
trzej_000 <trzeja.mikolaj@gmail.com>
2017-09-20 19:18:44 +0200

    One script to rule them all (perspektywa wsadzona do v6)

--- Clinic/DataLayer/Scripts/RegistrarPatientsUndVisits.sql
+++
@@ -1,8 +0,0 @@
-create view dbo.registrarVisitView as 
-SELECT dbo.Visit.id_visit, dbo.Patient.fname AS patientFname, dbo.Patient.lname AS patientLname, dbo.[User].fname AS doctorFname, dbo.[User].lname AS doctorLname, dbo.Visit.state, dbo.Visit.registration_date, 
-dbo.Visit.execution_cancel_datetime
-FROM dbo.Doctor INNER JOIN
-dbo.Visit ON dbo.Doctor.id_doc = dbo.Visit.id_doctor INNER JOIN
-dbo.Patient ON dbo.Visit.id_patient = dbo.Patient.id_patient INNER JOIN
-dbo.[User] ON dbo.Doctor.user_name = dbo.[User].user_name
-
